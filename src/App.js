1800e4381f67d389ba1315777835ce8e5147da09
Pobarul-islam <pobarul3@gmail.com>
2022-06-26 13:26:04 +0600

    some code added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import { useEffect, useState } from 'react';
 function App() {
   const [users, setUsers] = useState([])
   useEffect(()=>{
-    fetch('http://localhost:5000/user')
+    fetch('http://localhost:5000/users')
     .then(res => res.json())
     .then(data => setUsers(data))
   },[])
   return (
     <div className="App">
      <h3>My data count: {users.length}</h3>
+
+    <ul>
+      {
+        users.map(user=> <li key={user.id}>Namd: {user.name} Email: {user.email}</li>)
+      }
+    </ul>
     </div>
   );
 }
